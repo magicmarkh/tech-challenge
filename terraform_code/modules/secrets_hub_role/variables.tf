3311e343d2a537ba48c25697bc541973fa514ead
magicmarkh <mark.t.hurter@gmail.com>
2025-05-13 10:40:51 -0500

    secrets hub role creation

---
+++ terraform_code/modules/secrets_hub_role/variables.tf
@@ -0,0 +1,22 @@
+variable "Policy" {
+  description = "Meaningful policy name"
+  type        = string
+  default = "SecretsHubInstallPolicy"
+}
+
+variable "CyberArkSecretsHubRoleARN" {
+  description = "The Secrets Hub tenant role ARN which will be trusted by this role - get this from the cyberark tenant in secrets hub settings."
+  type        = string
+}
+
+variable "SecretsManagerRegion" {
+  description = "The AWS Secrets Manager Account region that the Secrets Hub will have access to"
+  type        = string
+  default = "us-east-1"
+}
+
+variable "AllowSecretExtendedAccess" {
+  description = "Allow Secrets Hub to retrieve secret values"
+  type        = bool
+  default     = true
+}
